42620c651ad21c687d749925fe47df95bc471304
ZakCrosser <zakcrosser@gmail.com>
2012-08-29 19:16:31 -0600

    Project 1

---
+++ Project 1/js/json.js
@@ -0,0 +1,185 @@
+var json = {
+  "transaction1": {
+    "date": ["Date", "2012-08-21"],
+    "transType": ["Type", "Deposit" ],
+    "catagory": ["Catagory", "Credit_Card"],
+    "amount": ["Amount", "500.00"],
+    "notes": ["Notes", "New Card"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+
+  "transaction2": {
+    "date": ["Date", "2012-08-22"],
+    "transType": ["Type", "Adjustment" ],
+    "catagory": ["Catagory:", "Food"],
+    "amount": ["Amount", "6.98"],
+    "notes": ["Notes", "Wendy's"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+
+  "transaction3": {
+    "date": ["Date", "2012-08-23"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "58.04"],
+    "notes": ["Notes", "Movies"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+
+  "transaction4": {
+    "date": ["Date", "2012-08-24"],
+    "transType": ["Type", "Transfer" ],
+    "catagory": ["Catagory", "ATM_Withdraw"],
+    "amount": ["Amount", "230.00"],
+    "notes": ["Notes", "Transfer to Checking"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction5": {
+    "date": ["Date", "2012-08-31"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "67.48"],
+    "notes": ["Notes", "Camera"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction6": {
+    "date": ["Date", "2012-08-26"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "54.76"],
+    "notes": ["Notes", "Pizza Party"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction7": {
+    "date": ["Date", "2012-08-24"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "ATM_Withdraw"],
+    "amount": ["Amount", "100.00"],
+    "notes": ["Notes", "Withdraw"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction8": {
+    "date": ["Date", "2012-08-12"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "45.67"],
+    "notes": ["Notes", "Bar"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction9": {
+    "date": ["Date", "2012-08-18"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "76.78"],
+    "notes": ["Notes", "Music Tickets"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction10": {
+    "date": ["Date", "2012-08-19"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "23.65"],
+    "notes": ["Notes", "Fridays"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction11": {
+    "date": ["Date", "2012-08-19"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "34.77"],
+    "notes": ["Notes", "Mall"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction12": {
+    "date": ["Date", "2012-08-23"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "98.75"],
+    "notes": ["Notes", "Home Depot"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction13": {
+    "date": ["Date", "2012-08-220"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Credit_Card"],
+    "amount": ["Amount", "9.84"],
+    "notes": ["Notes", "Service Charge"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction14": {
+    "date": ["Date", "2012-08-20"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Credit_Card"],
+    "amount": ["Amount", "50.00"],
+    "notes": ["Notes", "Payment"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "Yes"]
+  },
+  "transaction15": {
+    "date": ["Date", "2012-08-20"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "34.04"],
+    "notes": ["Notes", "Groceries"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction16": {
+    "date": ["Date", "2012-08-21"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "34.98"],
+    "notes": ["Notes", "Beer"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction17": {
+    "date": ["Date", "2012-08-20"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "1.95"],
+    "notes": ["Notes", "Drink"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction18": {
+    "date": ["Date", "2012-08-21"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "5.65"],
+    "notes": ["Notes", "Food"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction19": {
+    "date": ["Date", "2012-08-24"],
+    "transType": ["Type", "Deposit" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "234.67"],
+    "notes": ["Notes", "Pay Check"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction20": {
+    "date": ["Date", "2012-08-25"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "6.78"],
+    "notes": ["Notes", "Food"],
+    "slider": ["Slider", "50"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  }
+}
