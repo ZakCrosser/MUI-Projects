e846cbbb3848035d99609fb2c7faa6151312ca54
ZakCrosser <zakcrosser@gmail.com>
2012-09-06 22:13:48 -0600

    project 2 push
    changes to project 2 and 3 to fulfill requirements

---
+++ project 2/app_a/js/main.js
@@ -0,0 +1,264 @@
+//Project 2
+//MiU Term 1209
+//Zachary Crosser
+//Project 3
+//VFW Term 1208
+//Zachary Crosser
+window.addEventListener("DOMContentLoaded", function(){
+  
+  function r(x){
+    var theElement = document.getElementById(x);
+    return theElement;
+  }
+  
+  var makeDropMenu = function(){
+    var formTag = document.getElementsByTagName("form"),
+        selectLi = r('category'),
+        makeSelect = document.createElement('select');
+        makeSelect.setAttribute("id", "groups");
+    for(var i=0, j=catagory.length; i<j; i++){
+      var makeOption = document.createElement('option');
+      var optText = catagory[i];
+      makeOption.setAttribute("value", optText);
+      makeOption.innerHTML = optText;
+      makeSelect.appendChild(makeOption);
+    }
+    selectLi.appendChild(makeSelect);
+  }
+  
+  var getRadioType = function(){
+    var radios = document.forms[0].types;
+    for(var i=0; i<radios.length; i++){
+      if(radios[i].checked){
+        typeValue = radios[i].value;
+      }
+    }
+  }
+  
+  var getCheckBoxStatus = function(){
+    if(r('recurring').checked){
+      checkBoxValue = $('recurring').value;
+    }else{
+      checkBoxValue = "No";
+    }
+  }
+  
+  var controls = function(n){
+    switch(n){
+      case "on":
+        r('form').style.display = "none";
+        r('clearData').style.display ="inline";
+        r('showInfo').style.display ="none";
+        r('addNew').style.display ="inline";
+        break;
+      case "off":
+        r('form').style.display = "block";
+        r('clearData').style.display ="inline";
+        r('showInfo').style.display ="inline";
+        r('addNew').style.display ="none";
+        r("data").style.display ="none";
+      default:
+        return false;
+    }
+  }
+  
+  var storeTransaction = function(key){
+    if(!key){
+        var id          = Math.floor(Math.random()*100001);
+    }else{
+      id = key;
+    }
+    getRadioType();
+    getCheckBoxStatus();
+    var item            = {};
+        item.date       =["Date", r('date').value];
+        item.transType  =["Type", typeValue];
+        item.catagory   =["Catagory", r('groups').value];
+        item.amount     =["Amount", r('amount').value];
+        item.checkBox   =["Is it a reccuring transaction:", checkBoxValue];
+        item.notes      =["Notes", r('notes').value];
+    localStorage.setItem(id, JSON.stringify(item));
+    alert("Transaction Saved!");
+    return item
+  }
+  
+  var showData = function(){
+    controls("on");
+    if(localStorage.length === 0){
+      alert("There is no data in Local Storage, static data function added!")
+     autoFillData();
+     showData();
+    } else {
+      var makeDiv = document.createElement("div");
+      makeDiv.setAttribute("id", "data");
+      var makeList = r('storedTransactions');
+      makeList.innerHTML = "";
+      makeDiv.appendChild(makeList);
+      document.body.appendChild(makeDiv);
+      r("data").style.display ="block"
+      for(var i=0, len=localStorage.length; i<len; i++){
+        var makeLi = document.createElement('li');
+        var linksLi = document.createElement('li');
+        makeList.appendChild(makeLi);
+        var key = localStorage.key(i);
+        var value = localStorage.getItem(key);
+        var obj = JSON.parse(value);
+        var makeSubList = document.createElement('ul');
+        makeLi.appendChild(makeSubList);
+        makeLi.setAttribute('class', 'transaction ' + obj.catagory[1]);
+        for(var s in obj){
+          var makeSubLi = document.createElement('li');
+          makeSubList.appendChild(makeSubLi);
+          var optSubText = obj[s][0]+" "+obj[s][1];
+          makeSubLi.innerHTML = optSubText;
+          makeSubList.appendChild(linksLi);
+        }
+        makeItemLinks(localStorage.key(i), linksLi);
+      }
+    }
+  }
+  
+  var autoFillData = function (){
+    for(var j in json){
+      var id = Math.floor(Math.random()*100001);
+      localStorage.setItem(id, JSON.stringify(json[j]));
+    }  
+  }
+  
+  var makeItemLinks = function(key, linksLi){
+    var editLink = document.createElement('a');
+    editLink.href = "#";
+    editLink.key = key;
+    var editText = "Edit Transaction";
+    editLink.onclick = (function() {editItem(key);});
+    editLink.innerHTML = editText;
+    linksLi.appendChild(editLink);
+    var breakTag = document.createElement('br');
+    linksLi.appendChild(breakTag);
+    var deleteLink = document.createElement('a');
+    deleteLink.href = "#";
+    var deleteText = "Delete Transaction";
+    deleteLink.onclick = (function() {deleteItem(key);});
+    deleteLink.innerHTML = deleteText;
+    linksLi.appendChild(deleteLink);
+  }  
+  
+  var editItem = function(key){
+    var value = localStorage.getItem(key);
+    var item = JSON.parse(value);
+    var submit = r('submit');
+    controls("off");
+    r('date').value = item.date[1];
+    var radios = document.forms[0].types;
+    for(var i=0; i<radios.length; i++){
+      if(radios[i].value == "Deposit" && item.transType[1] == "Deposit"){
+        radios[i].setAttribute("checked", "checked");
+      }else if(radios[i].value == "Withdraw" && item.transType[1] == "Withdraw"){
+        radios[i].setAttribute("checked", "checked");
+      }else if(radios[i].value == "Adjustment" && item.transType[1] == "Adjustment"){
+        radios[i].setAttribute("checked", "checked");
+      }else if(radios[i].value == "Transfer" && item.transType[1] == "Transfer"){
+        radios[i].setAttribute("checked", "checked");
+      }  
+    }
+    if(item.checkBox[1] == "Yes"){
+      r('recurring').setAttribute("checked", "checked");
+    }
+    r('groups').value = item.catagory[1];
+    r('amount').value = item.amount[1];
+    r('notes').value = item.notes[1];
+    
+    submit.value = "Edit Transaction";
+    submit.key = key;
+    console.log('editItem');
+    console.log(submit.key);
+    submit.onclick = (function(evt) {validate(evt);});
+  }
+  
+  var deleteItem = function(key){
+    var ask = confirm("Are you sure you want to delete this Transaction?");
+    if(ask){
+      localStorage.removeItem(key);
+      window.location.reload();
+    }else{
+      alert("Transaction was NOT deleted!")
+    }
+  }
+  
+  var clearData = function(){
+    if(localStorage.length === 0){
+      alert("There Are No Transactions Saved!")
+    }else{
+      localStorage.clear();
+      alert("All Transactions Have Been Deleted!");
+      window.location.reload();
+      return false;
+    }
+  }
+  
+  var validate = function(evt){
+    console.log(r('submit').key);
+    var getDate      =r('date');
+    var getCatagory  =r('groups');
+    var getAmount    =r('amount');
+    var getNotes     =r('notes');
+    
+    errMsg.innerHTML = "";
+    getCatagory.setAttribute("class", "");
+    getDate.setAttribute("class", "");
+    getAmount.setAttribute("class", "");
+    getNotes.setAttribute("class", "");
+    
+  
+    
+    var messageAry = [];
+    
+    if(getCatagory.value === "--Choose a Catagory--"){
+      var catagoryError = "Please Pick a Catagory";
+      getCatagory.setAttribute("class", "errors");
+      messageAry.push(catagoryError); 
+    }
+    if(getDate.value === ""){
+      var dateError = "Please Enter A Date";
+      getDate.setAttribute("class", "errors");
+      messageAry.push(dateError);
+    }
+    if(!(new RegExp("^[0-9\.]+$", "i")).test(getAmount.value)){
+      var amountError = "Please Enter A Amount";
+      getAmount.setAttribute("class", "errors");
+      messageAry.push(amountError);
+    }
+    if(getNotes.value === ""){
+      var notesError = "Please Enter Notes";
+      getNotes.setAttribute("class", "errors");
+      messageAry.push(notesError);
+    }
+    if(messageAry.length >= 1){
+      for(var i=0, j=messageAry.length; i < j; i++){
+        var txt = document.createElement('li');
+        txt.innerHTML = messageAry[i];
+        errMsg.appendChild(txt);
+      }
+      evt.preventDefault();
+      return false;
+    }else{
+      return storeTransaction($('submit').key);
+    }
+  }
+ 
+ 
+    
+  var catagory = ["--Choose a Catagory--", "Food", "Credit_Card", "Entertainment", "ATM_Withdraw"],
+      typeValue,sss
+      checkBoxValue = "No"
+      errMsg = r('errors');
+      ;
+  makeDropMenu();
+
+  document.getElementById('showInfo').onclick = (function(evt) {showData(evt);});
+  document.getElementById('clearData').onclick = (function(evt) {clearData(evt);});
+  document.getElementById('submit').onclick = (function(evt) {validate(evt);});
+  
+  
+  
+});
---
+++ project 3/js/json.js
@@ -0,0 +1,165 @@
+var json = {
+  "transaction1": {
+    "date": ["Date", "2012-08-21"],
+    "transType": ["Type", "Deposit" ],
+    "catagory": ["Catagory", "Credit_Card"],
+    "amount": ["Amount", "500.00"],
+    "notes": ["Notes", "New Card"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+
+  "transaction2": {
+    "date": ["Date", "2012-08-22"],
+    "transType": ["Type", "Adjustment" ],
+    "catagory": ["Catagory:", "Food"],
+    "amount": ["Amount", "6.98"],
+    "notes": ["Notes", "Wendy's"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+
+  "transaction3": {
+    "date": ["Date", "2012-08-23"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "58.04"],
+    "notes": ["Notes", "Movies"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+
+  "transaction4": {
+    "date": ["Date", "2012-08-24"],
+    "transType": ["Type", "Transfer" ],
+    "catagory": ["Catagory", "ATM_Withdraw"],
+    "amount": ["Amount", "230.00"],
+    "notes": ["Notes", "Transfer to Checking"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction5": {
+    "date": ["Date", "2012-08-31"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "67.48"],
+    "notes": ["Notes", "Camera"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction6": {
+    "date": ["Date", "2012-08-26"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "54.76"],
+    "notes": ["Notes", "Pizza Party"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction7": {
+    "date": ["Date", "2012-08-24"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "ATM_Withdraw"],
+    "amount": ["Amount", "100.00"],
+    "notes": ["Notes", "Withdraw"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction8": {
+    "date": ["Date", "2012-08-12"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "45.67"],
+    "notes": ["Notes", "Bar"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction9": {
+    "date": ["Date", "2012-08-18"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "76.78"],
+    "notes": ["Notes", "Music Tickets"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction10": {
+    "date": ["Date", "2012-08-19"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "23.65"],
+    "notes": ["Notes", "Fridays"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction11": {
+    "date": ["Date", "2012-08-19"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "34.77"],
+    "notes": ["Notes", "Mall"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction12": {
+    "date": ["Date", "2012-08-23"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "98.75"],
+    "notes": ["Notes", "Home Depot"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction13": {
+    "date": ["Date", "2012-08-20"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Credit_Card"],
+    "amount": ["Amount", "9.84"],
+    "notes": ["Notes", "Service Charge"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction14": {
+    "date": ["Date", "2012-08-20"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Credit_Card"],
+    "amount": ["Amount", "50.00"],
+    "notes": ["Notes", "Payment"],
+    "checkBox": ["Is it a reccuring transation", "Yes"]
+  },
+  "transaction15": {
+    "date": ["Date", "2012-08-20"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "34.04"],
+    "notes": ["Notes", "Groceries"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction16": {
+    "date": ["Date", "2012-08-21"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "34.98"],
+    "notes": ["Notes", "Beer"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction17": {
+    "date": ["Date", "2012-08-20"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "1.95"],
+    "notes": ["Notes", "Drink"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction18": {
+    "date": ["Date", "2012-08-21"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "5.65"],
+    "notes": ["Notes", "Food"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction19": {
+    "date": ["Date", "2012-08-24"],
+    "transType": ["Type", "Deposit" ],
+    "catagory": ["Catagory", "Entertainment"],
+    "amount": ["Amount", "234.67"],
+    "notes": ["Notes", "Pay Check"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  },
+  "transaction20": {
+    "date": ["Date", "2012-08-25"],
+    "transType": ["Type", "Withdraw" ],
+    "catagory": ["Catagory", "Food"],
+    "amount": ["Amount", "6.78"],
+    "notes": ["Notes", "Food"],
+    "checkBox": ["Is it a reccuring transation", "No"]
+  }
+}
